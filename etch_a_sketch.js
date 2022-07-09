26c0a843890a5484ebf854fc9951eb10cf0738e2
Riuzzo01 <davide1.mariuzzo@gmail.com>
2022-07-09 18:25:44 +0200

    Fixed some bugs + Add the function to fill the grid clicking on the apposite butt

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,15 +1,36 @@
-
-
 function setGridDimension(dim) {
-    const container = document.querySelector(".container");
-    container.setAttribute("style", "display: grid; grid-template-row: repeat(" + dim + ", " + 500.0/dim + "px); grid-template-columns: repeat(" + dim + ", " + 500.0/dim + "px);");
+    const containerOld = document.querySelector("#container");
+    containerOld.remove()
+    const containerNew = document.createElement("div");
+    containerNew.id = "container";
+    containerNew.setAttribute("style", " margin: 0 auto; border: 3px solid red; width: 500px; height: 500px; background-color: aliceblue; display: grid; grid-template-rows: repeat(" + dim + ", " + 500.0/dim + "px); grid-template-columns: repeat(" + dim + ", " + 500.0/dim + "px);");
+    document.querySelector("body").appendChild(containerNew);
     for(let i = 1; i <= dim; i++) {
         for(let j = 1; j <= dim; j++) {
             const square = document.createElement("div");
             square.setAttribute("style", "grid-column-start: " + i + "; grid-row-start: " + j + "; grid-column-end: " + i + "; grid-row-end: " + j + "; border: 1px solid black; background-color: white"); 
-            container.appendChild(square);
+            containerNew.appendChild(square);
         }
     }
 }
 
-setGridDimension(13);
+function filler() {
+    document.querySelectorAll("#container div").forEach(element => { 
+        element.addEventListener("mouseover", () => {element.setAttribute("style", "background-color: black;");});
+    });
+}
+
+setGridDimension(30);
+
+const radio10 = document.querySelector("#radio10");
+const radio20 = document.querySelector("#radio20");
+const radio30 = document.querySelector("#radio30");
+
+radio10.addEventListener("click", () => {setGridDimension(10);});
+radio20.addEventListener("click", () => {setGridDimension(20);});
+radio30.addEventListener("click", () => {setGridDimension(30);});
+
+const color = document.querySelector("button");
+
+color.addEventListener("click", filler)
+
